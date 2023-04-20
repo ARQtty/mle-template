e92477718050c4eeffd15730f36f5b158926216b
ARQtty <ShamovIA@yandex.ru>
2023-04-20 23:38:24 +0300

    feat: add model notebook and code

---
+++ tests/test_model_quality.py
@@ -0,0 +1,11 @@
+import pytest
+from pathlib import Path
+from yamlparams import Hparam
+from src.bikes_regression import LinregModel
+
+
+def test_valid_save():
+    root = Path('..')
+    cfg = Hparam(root / 'config.yaml')
+    model = LinregModel()
+    model.load(root / cfg.model.path)
